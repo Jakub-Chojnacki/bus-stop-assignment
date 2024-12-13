8daf48b65131bac18942f650e5854ff8fcbd4f65
Jakub Chojnacki <jakub.chojnacki27@gmail.com>
2024-12-13 13:07:13 +0100

    reset selected bus stop and bus line on unmounting homeview

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,19 @@
 <script setup>
+import { onUnmounted } from "vue";
+import { useStore } from "@/store";
+import { ActionTypes } from "@/store/actions";
+
 import BusLineSelect from "@/components/BusLineSelect.vue";
 import MainLayout from "@/components/MainLayout.vue";
 import BusStopsGrid from "@/components/BusStopsGrid.vue";
+
+const store = useStore();
+
+onUnmounted(() => {
+  store.dispatch(ActionTypes.SET_SELECTED_BUS_STOP, null);
+  store.dispatch(ActionTypes.SET_SELECTED_BUS_LINE, null);
+});
+
 </script>
 
 <template>
